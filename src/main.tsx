026886213e9dbc36c30a6013b78f2b395917e284
MohanRaj Balumuri <mbalumuri@esri.com>
2025-03-10 14:40:37 -0700

    Fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/esri-devsummit-2025">
       <App />
     </BrowserRouter>
   </StrictMode>
